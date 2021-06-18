214b78bbdca6ccf0094a72abd7d4316fd21bc5f0
Mayurkumar patel <you@example.com>
2021-06-18 02:31:42 +0100

    Report update

--- src/test/java/com/June2021/RunCukeTest.java
+++ src/test/java/com/June2021/RunCukeTest.java
@@ -6,9 +6,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/Resources", tags = "@PriceRange", plugin = { "html: target/cucumber-html-report",
-        "json: target/cucumber-json-report.json",
-        "junit: target/cucumber-xml-report.xml",
-        "pretty: target/cucumber-pretty-report.txt"})
+@CucumberOptions(features = "src/test/Resources", tags = "@PriceRange",
+        plugin = { "html:target/cucumber-html-report",
+                "json:target/cucumber-json-report.json",
+                "junit:target/cucumber-xml-report.xml",
+                "pretty:target/cucumber-pretty-report.txt"})
 public class RunCukeTest {
 }
